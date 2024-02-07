ae0a91d1639e4f0da40a8f6ad09f493403fe6c8b
Hanka8 <hanka.maruskevicova@gmail.com>
2024-02-07 17:35:35 +0100

    loading simulation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Intro() {
   return (
       <div className='w-100 mh-100 flex-center'>
-        <img className='intro-item rotate' src="./public/images/loading.png" alt="" />
+        <img className='intro-item rotate' src="./images/loading.png" alt="" />
       </div>
   )
 }
